bf82a2b702d9a38596c51c55af7b593c7a8d466d
毕夏 <978260577@qq.com>
2025-03-05 22:00:05 +0800

    Initialize

--- Q_learning.py
+++ Q_learning.py
@@ -28,6 +28,7 @@ class Agent():
         for i in range(len(local_peaks)):
             self.reality[self.peak_indices[i + 1]] = local_peaks[i]
         self.state = [random.randint(0, 1) for _ in range(self.N)]
+        self.max_length = 5000  # make sure an episode can end with peaks
 
     def learn(self, tau=20, alpha=0.8, gamma=0.9):
         """
@@ -35,11 +36,12 @@ class Agent():
         Larger Tau: exploration (at 30, random walk);  Smaller Tau: exploitation
         :param tau: temperature regulates how sensitive the probability of choosing a given action is to the estimated Q
         :param state: current state, int
+        :param alpha: learning rate (cf. Denrell 2004)
+        :param gamma: emphasis on positional value (cf. Denrell 2004)
         :return:
         """
         # avoid unlimited search
-        max_step = 1000
-        for _ in range(max_step):
+        for _ in range(self.max_length):
             cur_state_index = int(''.join(map(str, self.state)), 2)
             # print(self.state, cur_state_index)
             q_row = self.Q_tabel[cur_state_index]
@@ -58,28 +60,32 @@ class Agent():
                 self.Q_tabel[cur_state_index][action] = (1 - alpha) * self.Q_tabel[cur_state_index][action] + alpha * reward
                 break
             else:
-                next_q_row = self.Q_tabel[next_state_index]
-                next_exp_prob_row = [np.exp(each / tau) for each in next_q_row]
-                next_prob_row = [each / sum(next_exp_prob_row) for each in next_exp_prob_row]
-                next_proper_action = np.random.choice(range(self.N + 1), p=next_prob_row)
-                next_state_quality = self.Q_tabel[next_state_index][next_proper_action]
+                # according to (Denrell, 2004), the next state quality/credit is the best state-action pair
+                next_state_quality = max(self.Q_tabel[next_state_index])
                 self.Q_tabel[cur_state_index][action] = (1 - alpha) * self.Q_tabel[cur_state_index][action] + alpha * (
                             reward + gamma * next_state_quality)
                 self.state = next_state
 
-    def perform(self, tau=20):
-        # re-initialize
-        self.state = [random.randint(0, 1) for _ in range(self.N)]
-        cur_state_index = int(''.join(map(str, self.state)), 2)
-        # print(self.state, cur_state_index)
-        q_row = self.Q_tabel[cur_state_index]
-        exp_prob_row = [np.exp(each / tau) for each in q_row]
-        prob_row = [each / sum(exp_prob_row) for each in exp_prob_row]
-        action = np.random.choice(range(self.N + 1), p=prob_row)
+    def perform(self, tau=20.0):
+        for _ in range(self.max_length):
+            # re-initialize
+            self.state = [random.randint(0, 1) for _ in range(self.N)]
+            cur_state_index = int(''.join(map(str, self.state)), 2)
+            # print(self.state, cur_state_index)
+            q_row = self.Q_tabel[cur_state_index]
+            exp_prob_row = [np.exp(each / tau) for each in q_row]
+            prob_row = [each / sum(exp_prob_row) for each in exp_prob_row]
+            action = np.random.choice(range(self.N + 1), p=prob_row)
 
-        next_state = self.state.copy()
-        next_state[action] = 1 - self.state[action]
-        self.state = next_state
+            next_state = self.state.copy()
+            if action < self.N:
+                next_state[action] = 1 - self.state[action]
+            next_state_index = int(''.join(map(str, next_state)), 2)
+            reward = self.reality[next_state_index]
+            self.state = next_state
+            if reward:
+                return reward
+
 
     def int_to_binary_list(self, state):
         return [int(bit) for bit in format(state, f'0{self.bit_length}b')]
@@ -125,14 +131,42 @@ class Agent():
 
         plt.show()
 
-        plt.show()
 
 if __name__ == '__main__':
     # 2 ^ 5 = 32 states
-    q_agent = Agent(N=10, global_peak=50, local_peaks=[10])
     # print(q_learn.Q_tabel)
     # print(q_agent.reality)
-    for i in range(500):
-        reward = q_agent.learn(tau=30, alpha=0.8, gamma=0.9)
-        if i % 50 == 0:
-            q_agent.visualize()
+    percentage_high_across_learning_length, percentage_low_across_learning_length = [], []
+    # [50, 100, 150, 200, 250, 300, 350]
+    learning_length_list = [100, 200, 300]
+    agent_num = 200
+    for learning_length in learning_length_list:
+        reward_across_agents = []
+        for _ in range(agent_num):
+            q_agent = Agent(N=10, global_peak=50, local_peaks=[10])
+            for _ in range(learning_length):
+                q_agent.learn(tau=20, alpha=0.8, gamma=0.9)
+            reward = q_agent.perform(tau=20)
+            reward_across_agents.append(reward)
+
+        percentage_high = sum([1 if reward == 50 else 0 for reward in reward_across_agents]) / agent_num
+        percentage_low = sum([1 if reward == 10 else 0 for reward in reward_across_agents]) / agent_num
+
+        percentage_high_across_learning_length.append(percentage_high)
+        percentage_low_across_learning_length.append(percentage_low)
+
+    plt.plot(learning_length_list, percentage_high_across_learning_length, "-", color='red', linewidth=2, label="High Peak")
+    plt.plot(learning_length_list, percentage_low_across_learning_length, "--", color='blue', linewidth=2, label="Low Peak")
+    # Add labels and title
+    plt.xlabel('Performance')
+    plt.ylabel('Learning Length')
+    plt.title('Performance Implications of Maximization vs. Softmax Strategies')
+
+    # Add grid and legend
+    plt.grid(True)
+    plt.legend()
+
+    # Show the plot
+    plt.show()
+
+
---
+++ run.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2/16/2025 20:08
+# @Author   : Junyi
+# @FileName: Q_learning.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+import numpy as np
+from Q_learning import Agent
+import multiprocessing as mp
+import time
+from multiprocessing import Semaphore
+import pickle
+
+if __name__ == '__main__':
+    # 2 ^ 5 = 32 states
+    # print(q_learn.Q_tabel)
+    # print(q_agent.reality)
+
+
+
+
+def func(learning_length=None, loop=None, return_dict=None, sema=None):
+    np.random.seed(None)
+    q_agent = Agent(N=10, global_peak=50, local_peaks=[10])
+    for _ in range(learning_length):
+        q_agent.learn(tau=20, alpha=0.8, gamma=0.9)
+    reward = q_agent.perform(tau=20)
+    return_dict[loop] = [reward]
+    sema.release()
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    concurrency = 50
+    repetition = 2000
+    learning_length_list = [50, 100, 150, 200, 250, 300, 350]
+    percentage_high_across_learning_length, percentage_low_across_learning_length = [], []
+    for learning_length in learning_length_list:
+        performance_list = []
+        sema = Semaphore(concurrency)
+        manager = mp.Manager()
+        jobs = []
+        return_dict = manager.dict()
+        for loop in range(repetition):
+            sema.acquire()
+            p = mp.Process(target=func, args=(learning_length, loop, return_dict, sema))
+            jobs.append(p)
+            p.start()
+        for proc in jobs:
+            proc.join()
+        results = return_dict.values()  # Don't need dict index, since it is repetition.
+        performance_list += [result[0] for result in results]
+
+        percentage_high = sum([1 if reward == 50 else 0 for reward in performance_list]) / repetition
+        percentage_low = sum([1 if reward == 10 else 0 for reward in performance_list]) / repetition
+
+        percentage_high_across_learning_length.append(percentage_high)
+        percentage_low_across_learning_length.append(percentage_low)
+
+    with open("highs_across_learning_length", 'wb') as out_file:
+        pickle.dump(percentage_high_across_learning_length, out_file)
+    with open("lows_across_learning_length", 'wb') as out_file:
+        pickle.dump(percentage_low_across_learning_length, out_file)
+
+    t1 = time.time()
+    print(time.strftime("%H:%M:%S", time.gmtime(t1 - t0)))  # Duration
+    print("Learning Rate:", time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time())))  # Complete time
+
