5ac7951e78de6db4906c00eb8e1ed1b11ff7f82b
毕夏 <978260577@qq.com>
2025-06-01 21:20:45 +0800

    Initialize: Figure 1-performance across learning length
    Figure 2-Informed across distance from low peaks

--- Turbulence/run_baseline.py
+++ Turbulence/run_baseline.py
@@ -23,16 +23,16 @@ def func(agent_num=None, learning_length=None, loop=None, return_dict=None, sema
     gamma = 0.9 # discount factor
     global_peak_value = 50 # as per (Fang, 2009)
     local_peak_value = 10
-    turbulence_freq = 50
-    turbulence_intensity = 0.3
+    # turbulence_freq = 50
+    turbulence_intensity = 0.4
     reality = Reality(N=N, global_peak_value=global_peak_value, local_peak_value=local_peak_value)
     organic_performance_list, organic_knowledge_list, organic_steps_list,organic_knowledge_quality_list = [], [], [], []
     for _ in range(agent_num):
         agent = Agent(N=N, reality=reality)
         for episode in range(learning_length):
-            if episode % turbulence_freq == 0:
-                reality.change(likelihood=turbulence_intensity)  # will also update the reality of agents
             agent.learn(tau=tau, alpha=alpha, gamma=gamma)
+        # after learning: change the reality
+        reality.change(likelihood=turbulence_intensity)
         agent.learn(tau=0.1, alpha=alpha, gamma=gamma, evaluation=True)  # evaluation
         organic_performance_list.append(agent.performance)
         organic_knowledge_list.append(agent.knowledge)
@@ -50,9 +50,9 @@ def func(agent_num=None, learning_length=None, loop=None, return_dict=None, sema
     for _ in range(agent_num):
         pair_agent = Agent(N=N, reality=reality)
         for episode in range(learning_length):
-            if episode % turbulence_freq == 0:
-                reality.change(likelihood=turbulence_intensity)  # will also update the reality of parrot and pair_agents
             pair_agent.learn_with_parrot(tau=tau, alpha=alpha, gamma=gamma, parrot=parrot, valence=50)
+        # after learning: change the reality
+        reality.change(likelihood=turbulence_intensity)
         pair_agent.learn_with_parrot(tau=0.1, alpha=alpha, gamma=gamma, parrot=parrot, valence=50, evaluation=True) # evaluation
         pair_performance_list.append(pair_agent.performance)
         pair_knowledge_list.append(pair_agent.knowledge)
@@ -73,8 +73,8 @@ if __name__ == '__main__':
     t0 = time.time()
     concurrency = 50
     agent_num = 100
-    repetition = 50
-    learning_length_list = [200, 300, 400, 500]
+    repetition = 100
+    learning_length_list = [50, 100, 150, 200, 250, 300, 350, 400, 450]
     organic_performance_across_episodes, organic_knowledge_across_episodes, organic_steps_across_episodes, organic_knowledge_quality_across_episodes = [], [], [], []
     pair_performance_across_episodes, pair_knowledge_across_episodes, pair_steps_across_episodes, pair_knowledge_quality_across_episodes = [], [], [], []
     for learning_length in learning_length_list:
